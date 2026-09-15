d089e7d5a4ca78a4760a97b8bcf569889d235f82
jesus <negoprojss@gmail.com>
2026-09-14 21:53:28 -0600

    funcion de listar clientes desde la base de datos

--- src/main/java/zona_fit/datos/ClienteDAO.java
+++ src/main/java/zona_fit/datos/ClienteDAO.java
@@ -1,15 +1,47 @@
 package zona_fit.datos;
 
+import zona_fit.conexion.Conexion;
 import zona_fit.dominio.Cliente;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ClienteDAO implements  IClienteDAO{
+import static zona_fit.conexion.Conexion.getConexion;
 
+
+public class ClienteDAO implements  IClienteDAO{
     @Override
     public List<Cliente> listaCliente() {
-
-        return List.of();
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        var sql = "SELECT * FROM cliente ORDER BY ID";
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                var cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMenbrecia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+        }catch (Exception e){
+            System.out.println("Error al consultar");
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar conexion");
+            }
+        }
+        return clientes;
     }
 
     @Override
@@ -30,5 +62,13 @@ public class ClienteDAO implements  IClienteDAO{
     @Override
     public boolean eliminarCliente(Cliente cliente) {
         return false;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("++ lita cleente++");
+        IClienteDAO clienteDAO = new ClienteDAO();
+        var clientes = clienteDAO.listaCliente();
+        clientes.forEach(System.out::println);
     }
 }
